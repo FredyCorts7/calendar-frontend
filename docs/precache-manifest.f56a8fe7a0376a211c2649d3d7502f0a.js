3470a02211d3615d13bee81d1f51bbe9636f75a4
fredycorts7 <fred.cor.14@gmail.com>
2020-09-28 23:38:15 -0500

    <ADD> preparacion para desplegar a github pages

---
+++ docs/precache-manifest.f56a8fe7a0376a211c2649d3d7502f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd292fb2e958583376fe515134c59511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e96ec5576229d80c6a",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "bb1289bca5c6f36e5228",
+    "url": "/static/css/main.5d4a597f.chunk.css"
+  },
+  {
+    "revision": "d9e96ec5576229d80c6a",
+    "url": "/static/js/2.60b84426.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.60b84426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1289bca5c6f36e5228",
+    "url": "/static/js/main.cb0ddaa4.chunk.js"
+  },
+  {
+    "revision": "710b9ced68fde07e32a7",
+    "url": "/static/js/runtime-main.7bd248be.js"
+  }
+]);
